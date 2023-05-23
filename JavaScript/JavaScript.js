8b935d30030a15bfac1469c45f369bd730addb95
Deenk22 <smj_personal@hotmail.com>
2023-05-23 15:48:38 +0200

    Punto de partida

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,5 @@
+// Añadir las tarjetas con un bucle FOR innerHTML += y una tarjeta completa desde imagen hasta boton. Recuerda hacer la funcion de inicializacion
+// y crear el array de objetos de las tarjetas con sus propiedades correspondientes. Mira el proyecto de JavaScript Avanzado 
+// para obtener un poco de ayuda y recuerda... hacer lo de la contraseña y el email ... al iniciar sesion.
+
+// 
